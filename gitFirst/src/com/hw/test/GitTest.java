a102717e297aac1dacba3c83dad2e77fbf165b14
hivi <hewensvip@163.com>
2019-06-16 16:11:52 +0800

    拉取之后提交

--- gitFirst/src/com/hw/test/GitTest.java
+++ gitFirst/src/com/hw/test/GitTest.java
@@ -2,10 +2,13 @@ package com.hw.test;
 
 public class GitTest {
     public static void main(String[] args) {
+        //拉下来的继续提交
         System.out.println(1111111111);
+        System.out.println(222222222);
     }
 
     public static void show() {
         System.out.println(2222);
+        System.out.println(333333333);
     }
 }
